9c43b38f20c8702650629186c58d15a5a73aee83
Jaswanthn1 <jaswanth.kotakonda@gmail.com>
2021-12-07 13:43:38 +0530

    initial_commit

---
+++ c6-01-vnet-input-variables.tf
@@ -0,0 +1,27 @@
+# Virtual Network, Subnets and Subnet NSG's
+
+## Virtual Network
+variable "vnet_name" {
+  description = "Virtual Network name"
+  type = string
+  default = "vnet-default"
+}
+variable "vnet_address_space" {
+  description = "Virtual Network address_space"
+  type = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+# Web Subnet Name
+variable "web_subnet_name" {
+  description = "Virtual Network Web Subnet Name"
+  type = string
+  default = "websubnet"
+}
+# Web Subnet Address Space
+variable "web_subnet_address" {
+  description = "Virtual Network Web Subnet Address Spaces"
+  type = list(string)
+  default = ["10.0.1.0/24"]
+}
+
